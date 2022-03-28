08495f265425b2056fa73aad951dbec1bcca8470
b3r3n1c3 <joffreynuijten@gmail.com>
2022-03-28 15:42:48 +0200

    Toevoegen update

--- DemoGridView/MainWindow.xaml.cs
+++ DemoGridView/MainWindow.xaml.cs
@@ -45,13 +45,12 @@ namespace DemoGridView
             _Team.Add(Nip);
             _Team.Add(Astralis);
 
-
+            //Add items to Datagrid 
             DataGrid1.Items.Add(_Team[0]);
             DataGrid1.Items.Add(_Team[1]);
             DataGrid1.Items.Add(_Team[2]);
 
             // Add items to Combobox for Delete (Dropdown)
-
             CB1.Items.Add(_Team[0].TeamName);
             CB1.Items.Add(_Team[1].TeamName);
             CB1.Items.Add(_Team[2].TeamName);
@@ -91,20 +90,76 @@ namespace DemoGridView
         private void DeleteTeamButton_Click(object sender, RoutedEventArgs e)
         {
             // Here comes the program for deleting class instances 
-
             int tempIx = CB1.SelectedIndex;
-
             DataGrid1.Items.Remove(_Team[tempIx]);
             CB1.Items.Remove(CB1.SelectedValue);
+            CB2.Items.Remove(CB1.SelectedValue);
+            DataGrid1.Items.Refresh();
 
         }
 
         private void TeamInfoButton_Click(object sender, RoutedEventArgs e)
         {
-            // Navigate to Window with teeam info 
+            // Navigate to Window with team info 
             var newForm = new TeamInfo();
             newForm.Show();
             this.Close(); 
+        }
+
+        private void UpdateTeamButton_Click(object sender, RoutedEventArgs e)
+        {
+            Update_Error_TN.Text = "";
+            Update_Error_SY.Text = "";
+
+            // Check if team is selected in Combobox
+            if (string.IsNullOrEmpty(CB2.Text))
+            {
+                Update_Error_TN.Text = "No team selected.";
+            }
+            else
+            {
+
+                // Check if Year is inserted
+                if (string.IsNullOrEmpty(TB_Update_SY.Text))
+                {
+                    Update_Error_SY.Text = "No year inserted, no update";
+                }
+
+                else
+                {
+                    // Update Team Name 
+                    int tempIx = CB2.SelectedIndex;
+                    _Team[tempIx].StartYear = int.Parse(TB_Update_SY.Text);
+
+                    //Refresh Values
+                    DataGrid1.Items.Refresh();
+                }
+
+                // Check if Name is inserted
+                if (string.IsNullOrEmpty(TB_Update_TN.Text))
+                {
+                    Update_Error_TN.Text = "No name inserted, no update";
+                }
+                else
+                {
+                    // Update Team Name 
+                    int tempIx = CB2.SelectedIndex;
+                    _Team[tempIx].TeamName = TB_Update_TN.Text;
+
+                    //Refresh Values
+                    DataGrid1.Items.Refresh();
+
+                    CB1.Items.Remove(CB2.SelectedValue);
+                    CB1.Items.Add(_Team[tempIx].TeamName);
+
+                    CB2.Items.Remove(CB2.SelectedValue);
+                    CB2.Items.Add(_Team[tempIx].TeamName);
+                }
+
+
+            }
+
+
         }
     }
 }
--- DemoGridView/TeamInfo.xaml.cs
+++ DemoGridView/TeamInfo.xaml.cs
@@ -18,10 +18,14 @@ namespace DemoGridView
     public partial class TeamInfo : Window
     {
         public List<TeamMember> _Teammember;
+        public List<Team> _Team;
 
         public TeamInfo()
         {
             InitializeComponent();
+
+
+            // Declare teams 
             Team Navi = new Team();
             Navi.TeamName = "Natus Vincere";
             Navi.StartYear = 2009;
@@ -33,49 +37,92 @@ namespace DemoGridView
             Team Astralis = new Team();
             Astralis.TeamName = "Astralis";
             Astralis.StartYear = 2016;
-
+            
+            // Declare TeamMembers 
             TeamMember Simple = new TeamMember();
-           // Simple.Team = Navi;
-            Simple.TeamName = Navi.TeamName;
+            Simple.Team = Navi;
             Simple.MemberName = "Oleksandr Kostyliev";
             Simple.MemberAge = 24;
             Simple.MemberInGameName = "S1mple";
 
             TeamMember GetRight = new TeamMember();
             GetRight.Team = Nip;
-            GetRight.TeamName = Nip.TeamName;
-            GetRight.StartYear = Nip.StartYear;
             GetRight.MemberName = "Christopher Alesund";
             GetRight.MemberAge = 31;
             GetRight.MemberInGameName = "GeT_RiGhT";
 
             TeamMember Device = new TeamMember();
-            // Device.TeamName = "Astralis";
-            // Device.StartYear = 2016;
             Device.Team = Astralis;
             Device.MemberName = "Nicolai Hvilshøj Reedtz";
             Device.MemberAge = 26;
             Device.MemberInGameName = "dev1ce";
 
-
+            // Create list of Teammembers Classes
             _Teammember = new List<TeamMember>();
             _Teammember.Add(Simple);
             _Teammember.Add(GetRight);
             _Teammember.Add(Device);
 
+            // Add a list with Team classes
+            _Team = new List<Team>();
+            _Team.Add(Navi);
+            _Team.Add(Nip);
+            _Team.Add(Astralis);
+
+            // Add to grid 
             DataGrid2.Items.Add(_Teammember[0]);
             DataGrid2.Items.Add(_Teammember[1]);
             DataGrid2.Items.Add(_Teammember[2]);
 
+            // Add items to Combobox for adding teammember 
+            CB_Add_TN.Items.Add(_Team[0].TeamName);
+            CB_Add_TN.Items.Add(_Team[1].TeamName);
+            CB_Add_TN.Items.Add(_Team[2].TeamName);
 
+            // Add items to Comboxbox for deleting teammember
+            CB_Delete_TM.Items.Add(_Teammember[0].MemberInGameName);
+            CB_Delete_TM.Items.Add(_Teammember[1].MemberInGameName);
+            CB_Delete_TM.Items.Add(_Teammember[2].MemberInGameName);
 
 
         }
 
+        private void BT_Add_TM_Click(object sender, RoutedEventArgs e)
+        {
+            TeamMember tempTeammember = new TeamMember();
+            int tempInt = CB_Add_TN.SelectedIndex;
+            tempTeammember.Team = _Team[tempInt];
+            tempTeammember.MemberName = TBox_Add_MN.Text;
+            tempTeammember.MemberAge = int.Parse(TBox_Add_Age.Text);
+            tempTeammember.MemberInGameName = TBox_Add_IGN.Text;
 
+            // Add Teammember To the List _Teammembers
+            _Teammember.Add(tempTeammember);
+            DataGrid2.Items.Add(tempTeammember);
 
+            // Add Teammember to Combobox _Teammembers
+            CB_Delete_TM.Items.Add(tempTeammember.MemberInGameName);
 
+        }
 
+        private void BT_Delete_TM_Click(object sender, RoutedEventArgs e)
+        {
+            int tempIx = CB_Delete_TM.SelectedIndex;
+            DataGrid2.Items.Remove(_Teammember[tempIx]);
+            CB_Delete_TM.Items.Remove(CB_Delete_TM.SelectedValue);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var newForm = new MainWindow();
+            newForm.Show();
+            this.Close();
+        }
+
+        private void DataGrid2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 
 }
